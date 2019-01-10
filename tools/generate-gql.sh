2f52c93ad5e278c02e82c2ff4ad7f7b83628f6d2
Henry Hwangbo <henry74@gmail.com>
2019-01-09 22:14:05 -0600

    Merge conflicts

--- tools/generate-gql.sh
+++ tools/generate-gql.sh
@@ -3,16 +3,8 @@
 # ROOT_DIR='.'
 PORT='9898'
 
-<<<<<<< HEAD
-PORT=$PORT ts-node src/index.ts &
-SERVER_PID=$!
-
-sleep 4s
-
-=======
 PORT=$PORT ts-node src/index.ts & SERVER_PID=$!
 sleep 3
->>>>>>> 11be0033ec10b7f8612ce28a0beddbbfdbb4a5f7
 gql-gen --config ./tools/codegen.yml 
 
 kill -9 $SERVER_PID
